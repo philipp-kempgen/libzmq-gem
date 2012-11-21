6ab07915c2c7bcb1429f06f2c814943a917c7b82
Philipp Kempgen <lists@kempgen.net>
2012-11-21 11:28:08 +0100

    libzmq.{so|dylib}

--- test.rb
+++ test.rb
@@ -1,4 +1,7 @@
-gem 'libzmq'
+# Put our lib dir on top of the load path:
+$LOAD_PATH.unshift "#{File.dirname(__FILE__)}/lib"
+
+require 'libzmq'
 require 'libzmq/ffi-rzmq'
 
 puts "Using gem ZMQ: #{ZMQ::Util.version.join('.')}"
